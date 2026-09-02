a91e2e998cc6e0dd5eecf37ada330c578c7538b0
RJ <rybertjen@gmail.com>
2026-09-01 20:59:59 -0700

    build: add repository Nix toolchain

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Reproducible development and CI toolchain for CareerOps";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.bash
+              pkgs.coreutils
+              pkgs.gh
+              pkgs.git
+              pkgs.mise
+              pkgs.nodejs_22
+            ];
+          };
+        });
+    };
+}
